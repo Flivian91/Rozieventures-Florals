fb0e98569236353ab8010e0c43ee7dd46a65c65d
Flivian91 <fliviandev@gmail.com>
2025-09-04 00:08:02 +0300

    commit

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>All product listing</div>;
+  return <div>Cart</div>;
 }
 
 export default page;
--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>All product listing</div>;
+  return <div>Checkout page</div>;
 }
 
 export default page;
