dbe3b89214d9790d80eb95cba03b9630d1dd13a8
Flivian91 <fliviandev@gmail.com>
2025-09-04 21:29:16 +0300

    commit

--- src/components/landing/home/Hero.jsx
+++ src/components/landing/home/Hero.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Star } from "lucide-react";
 import heroImage from "../../../../public/images/hero-florals.jpg";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-  <section>
-    
-  </section>
+    <section className="min-h-screen bg-primary/60 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div></div>
+      <div>
+        <Image
+          src={"/images/hero.svg"}
+          height={100}
+          width={100}
+          alt="Hero Image"
+          className="h-full w-full"
+        />
+      </div>
+    </section>
   );
 };
 
