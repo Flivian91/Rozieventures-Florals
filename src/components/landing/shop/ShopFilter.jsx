7614af2b1a3b8e022613f1a92ffce2bbdb700b34
Flivian91 <fliviandev@gmail.com>
2025-09-12 00:00:08 +0300

    commit

--- src/components/landing/shop/ShopFilter.jsx
+++ src/components/landing/shop/ShopFilter.jsx
@@ -9,6 +9,7 @@ function ShopFilter() {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 py-4 mt-5 gap-5 items-center justify-center w-full">
       {/* Search bar */}
       <div className="">
+      {/* // This line */}
         <SHopSearchBar />
       </div>
       {/* Search by category */}
