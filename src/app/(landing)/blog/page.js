82984646e89e98afb08e0e0d5ce8cd2dff6efdda
Flivian91 <fliviandev@gmail.com>
2025-10-24 23:37:36 +0300

    commit

--- src/app/(landing)/blog/page.js
+++ src/app/(landing)/blog/page.js
@@ -1,7 +1,22 @@
+import PageHeading from "@/components/common/PageHeading";
 import React from "react";
 
 function page() {
-  return <div>Blog page</div>;
+  return (
+    <div>
+      {/* Introduction */}
+      <div className="bg-primary/20 py-10">
+        <PageHeading
+          heading={"Our Story"}
+          subHeading={
+            "Discover the art, science, and beauty of flowers through our expert insights, tips, and stories from the world of floriculture."
+          }
+        />
+      </div>
+      {/* Blogs collection */}
+      {/* Newsletter */}
+    </div>
+  );
 }
 
 export default page;
